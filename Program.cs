0edd993d1eb867abff576e23db65b33dd1ab216f
Mahmut Bilir <mahmut-bilir@hotmail.com>
2025-04-06 10:52:06 +0300

    Read from appsettings.json

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+
+//Read configuration from appsetting.json
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+// Değerleri oku
+var appName = configuration["AppSettings:ApplicationName"];
+var version = configuration["AppSettings:Version"];
+var maxRetry = configuration["AppSettings:MaxRetry"];
+
+Console.WriteLine($"Uygulama Adı: {appName}");
+Console.WriteLine($"Versiyon: {version}");
+Console.WriteLine($"MaxRetry: {maxRetry}");
