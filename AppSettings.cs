64259d9878e7f305c60627b8eb0a95bebf0d11ee
Mahmut Bilir <mahmut-bilir@hotmail.com>
2025-04-06 23:09:31 +0300

    Bind the configuration directly to a class

---
+++ AppSettings.cs
@@ -0,0 +1,6 @@
+public class AppSettings
+{
+    public string? ApplicationName { get; set; }
+    public string? Version { get; set; }
+    public int MaxRetry { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,18 +1,22 @@
-﻿using System;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 
 
-//Read configuration from appsetting.json
+// //Read configuration from appsetting.json
 
 var configuration = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
     .Build();
 
-// Read values
-var appName = configuration["AppSettings:ApplicationName"];
-var version = configuration["AppSettings:Version"];
-var maxRetry = configuration["AppSettings:MaxRetry"];
+// // Read values
+// var appName = configuration["AppSettings:ApplicationName"];
+// var version = configuration["AppSettings:Version"];
+// var maxRetry = configuration["AppSettings:MaxRetry"];
 
-Console.WriteLine($"Application name: {appName}");
-Console.WriteLine($"Version: {version}");
-Console.WriteLine($"MaxRetry: {maxRetry}");
+// Console.WriteLine($"Application name: {appName}");
+// Console.WriteLine($"Version: {version}");
+// Console.WriteLine($"MaxRetry: {maxRetry}");
+
+var appSettings = new AppSettings();
+configuration.GetSection("AppSettings").Bind(appSettings);
+
+Console.WriteLine($"Application : {appSettings.ApplicationName} | Version : {appSettings.Version}");
